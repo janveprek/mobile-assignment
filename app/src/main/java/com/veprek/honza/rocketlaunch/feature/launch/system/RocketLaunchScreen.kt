6c5f753c4cdb9bbc65be6da7959bb15c0978555d
janveprek <janveprek@gmail.com>
2022-07-25 11:07:09 +0200

    FEAT: Added RocketLaunchImpl

--- app/src/main/java/com/veprek/honza/rocketlaunch/feature/launch/system/RocketLaunchScreen.kt
+++ app/src/main/java/com/veprek/honza/rocketlaunch/feature/launch/system/RocketLaunchScreen.kt
@@ -5,43 +5,15 @@ import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.core.FastOutLinearInEasing
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.slideOut
-import androidx.compose.animation.slideOutVertically
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.IntOffset
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
-import com.veprek.honza.rocketlaunch.R
 import com.veprek.honza.rocketlaunch.feature.launch.presentation.RocketLaunchViewModel
 import com.veprek.honza.rocketlaunch.navigation.NavigationScreens
-import com.veprek.honza.rocketlaunch.repository.model.RocketState
-import com.veprek.honza.rocketlaunch.ui.theme.verticalPadding
 import quanti.com.kotlinlog.Log
 import kotlin.math.abs
 
@@ -53,7 +25,8 @@ fun RocketLaunchScreen(
     val viewModel: RocketLaunchViewModel = hiltViewModel()
 
     val context = LocalContext.current
-    val sensorManager: SensorManager = context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    val sensorManager: SensorManager =
+        context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
 
     var gravity: FloatArray? = null
     var geomagnetic: FloatArray? = null
@@ -114,97 +87,10 @@ fun RocketLaunchScreen(
         )
     }
 
-    Log.d("LaunchScreen")
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = { Text(stringResource(R.string.icon_launch)) },
-                navigationIcon = {
-                    IconButton(onClick = {
-                        navController.popBackStack(NavigationScreens.RocketDetailScreen.route, inclusive = false)
-                    }) {
-                        Icon(
-                            imageVector = Icons.Filled.ArrowBack,
-                            contentDescription = stringResource(R.string.icon_back)
-                        )
-                    }
-                }
-            )
-        }
-    ) {
-        Column(
-            verticalArrangement = Arrangement.Bottom,
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier.fillMaxSize()
-        ) {
-            val angle by when (state) {
-                RocketState.WAITING, RocketState.LAUNCHED -> {
-                    animateFloatAsState(
-                        targetValue = 0f
-                    )
-                }
-                RocketState.FAILED -> {
-                    animateFloatAsState(
-                        targetValue = 90f,
-                        animationSpec = tween(durationMillis = 1000)
-                    )
-                }
-            }
-            val exitTransition = when (state) {
-                RocketState.WAITING, RocketState.LAUNCHED -> {
-                    slideOutVertically(
-                        targetOffsetY = { -1500 },
-                        animationSpec = tween(
-                            durationMillis = 1000,
-                            easing = FastOutLinearInEasing
-                        )
-                    )
-                }
-                RocketState.FAILED -> {
-                    slideOut(
-                        targetOffset = { IntOffset(500, -100) },
-                        animationSpec = tween(
-                            durationMillis = 1000,
-                            easing = FastOutLinearInEasing
-                        )
-                    )
-                }
-            }
-            val image = when (state) {
-                RocketState.WAITING, RocketState.FAILED -> {
-                    painterResource(R.drawable.rocket_idle)
-                }
-                RocketState.LAUNCHED -> {
-                    painterResource(R.drawable.rocket_flying)
-                }
-            }
-            val text = when (state) {
-                RocketState.WAITING -> {
-                    stringResource(R.string.launch_initial)
-                }
-                RocketState.LAUNCHED -> {
-                    stringResource(R.string.launch_successful)
-                }
-                RocketState.FAILED -> {
-                    stringResource(R.string.launch_failed)
-                }
-            }
-
-            AnimatedVisibility(
-                visible = launched.value,
-                modifier = Modifier.rotate(angle),
-                exit = exitTransition
-            ) {
-                Image(
-                    painter = image,
-                    contentDescription = stringResource(R.string.icon_rocket)
-                )
-            }
-            Text(
-                modifier = Modifier.padding(bottom = verticalPadding),
-                text = text,
-                style = MaterialTheme.typography.body1
-            )
-        }
-    }
+    RocketLaunchScreenImpl(state = state, launched = launched.value, backAction = {
+        navController.popBackStack(
+            NavigationScreens.RocketDetailScreen.route,
+            inclusive = false
+        )
+    })
 }
---
+++ app/src/main/java/com/veprek/honza/rocketlaunch/feature/launch/system/RocketLaunchScreenImpl.kt
@@ -0,0 +1,133 @@
+package com.veprek.honza.rocketlaunch.feature.launch.system
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.FastOutLinearInEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.slideOut
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.IntOffset
+import com.veprek.honza.rocketlaunch.R
+import com.veprek.honza.rocketlaunch.repository.model.RocketState
+import com.veprek.honza.rocketlaunch.ui.theme.verticalPadding
+import quanti.com.kotlinlog.Log
+
+@Composable
+fun RocketLaunchScreenImpl(
+    state: RocketState,
+    launched: Boolean,
+    backAction: () -> Unit = {}
+) {
+    Log.d("LaunchScreen")
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(stringResource(R.string.icon_launch)) },
+                navigationIcon = {
+                    IconButton(onClick = { backAction() }) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = stringResource(R.string.icon_back)
+                        )
+                    }
+                }
+            )
+        }
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Bottom,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            val angle by when (state) {
+                RocketState.WAITING, RocketState.LAUNCHED -> {
+                    animateFloatAsState(
+                        targetValue = 0f
+                    )
+                }
+                RocketState.FAILED -> {
+                    animateFloatAsState(
+                        targetValue = 90f,
+                        animationSpec = tween(durationMillis = 1000)
+                    )
+                }
+            }
+            val exitTransition = when (state) {
+                RocketState.WAITING, RocketState.LAUNCHED -> {
+                    slideOutVertically(
+                        targetOffsetY = { -1500 },
+                        animationSpec = tween(
+                            durationMillis = 1000,
+                            easing = FastOutLinearInEasing
+                        )
+                    )
+                }
+                RocketState.FAILED -> {
+                    slideOut(
+                        targetOffset = { IntOffset(500, -100) },
+                        animationSpec = tween(
+                            durationMillis = 1000,
+                            easing = FastOutLinearInEasing
+                        )
+                    )
+                }
+            }
+            val image = when (state) {
+                RocketState.WAITING, RocketState.FAILED -> {
+                    painterResource(R.drawable.rocket_idle)
+                }
+                RocketState.LAUNCHED -> {
+                    painterResource(R.drawable.rocket_flying)
+                }
+            }
+            val text = when (state) {
+                RocketState.WAITING -> {
+                    stringResource(R.string.launch_initial)
+                }
+                RocketState.LAUNCHED -> {
+                    stringResource(R.string.launch_successful)
+                }
+                RocketState.FAILED -> {
+                    stringResource(R.string.launch_failed)
+                }
+            }
+
+            AnimatedVisibility(
+                visible = launched,
+                modifier = Modifier.rotate(angle),
+                exit = exitTransition
+            ) {
+                Image(
+                    painter = image,
+                    contentDescription = stringResource(R.string.icon_rocket)
+                )
+            }
+            Text(
+                modifier = Modifier.padding(bottom = verticalPadding),
+                text = text,
+                style = MaterialTheme.typography.body1
+            )
+        }
+    }
+}
