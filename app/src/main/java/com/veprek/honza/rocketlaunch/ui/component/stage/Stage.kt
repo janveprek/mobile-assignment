9741f0bb8830bbe48bea60746e4de4daf6a3af52
janveprek <janveprek@gmail.com>
2022-07-28 11:21:31 +0200

    FEAT: Added string plurals

--- app/src/main/java/com/veprek/honza/rocketlaunch/ui/component/stage/Stage.kt
+++ app/src/main/java/com/veprek/honza/rocketlaunch/ui/component/stage/Stage.kt
@@ -7,8 +7,10 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.pluralStringResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.veprek.honza.rocketlaunch.R
@@ -20,6 +22,7 @@ import com.veprek.honza.rocketlaunch.ui.theme.lightGray
 import com.veprek.honza.rocketlaunch.ui.theme.mediumPadding
 import com.veprek.honza.rocketlaunch.ui.theme.smallPadding
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun Stage(
     modifier: Modifier = Modifier,
@@ -47,9 +50,30 @@ fun Stage(
                     name = stringResource(R.string.stage_not_reusable)
                 )
             }
-            StageItem(icon = painterResource(R.drawable.engine), name = stringResource(R.string.stage_engine, stage.engines))
-            StageItem(icon = painterResource(R.drawable.fuel), name = stringResource(R.string.stage_fuel, stage.fuelAmountTons))
-            StageItem(icon = painterResource(R.drawable.burn), name = stringResource(R.string.stage_burn_time, stage.burnTimeSec))
+            StageItem(
+                icon = painterResource(R.drawable.engine),
+                name = pluralStringResource(
+                    id = R.plurals.stage_engine,
+                    count = stage.engines,
+                    stage.engines
+                )
+            )
+            StageItem(
+                icon = painterResource(R.drawable.fuel),
+                name = pluralStringResource(
+                    R.plurals.stage_fuel,
+                    count = stage.fuelAmountTons,
+                    stage.fuelAmountTons
+                )
+            )
+            StageItem(
+                icon = painterResource(R.drawable.burn),
+                name = pluralStringResource(
+                    R.plurals.stage_burn_time,
+                    count = stage.burnTimeSec,
+                    stage.burnTimeSec
+                )
+            )
         }
     }
 }
